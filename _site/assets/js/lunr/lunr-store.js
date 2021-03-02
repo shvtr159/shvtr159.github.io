8b3b0ca7b02f9ef3fb16aa04a12ae2f33d638141
shvtr159 <55650284+shvtr159@users.noreply.github.com>
2021-03-02 19:22:20 +0900

    Harris

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Harris corner detector",
-        "excerpt":"이미지에서 코너는 edge 등에 비해 noise와 같은 문제에도 안정적으로 descriptor의 역할을 할 수 있다. 이 때 corner를 찾기 위해서 사용하는 알고리즘 중 harris corner detector에 대해서 알아본다. 1. Basic idea flat : 지역에서는 모든 방향으로 변화가 없다 edge : 영역에서는 edge 방향으로는 변화가 없으나 이에 수직한 방향으로는 변화가 있다. corner...","categories": ["ComputerVision"],
+        "excerpt":"이미지에서 코너는 edge 등에 비해 noise와 같은 문제에도 안정적으로 descriptor의 역할을 할 수 있습니다. 이 때 corner를 찾기 위해서 사용하는 알고리즘 중 harris corner detector에 대해서 알아봅니다. 1. Basic idea flat : 지역에서는 모든 방향으로 변화가 없음 edge : 영역에서는 edge 방향으로는 변화가 없으나 이에 수직한 방향으로는 변화가 있음. corner...","categories": ["ComputerVision"],
         "tags": [],
         "url": "/computervision/harris/",
         "teaser": null
