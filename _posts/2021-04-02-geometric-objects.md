a1be078fafd0f6980739ec47f59af94047d08988
kyg <shvtr159@gmail.com>
2021-04-09 03:07:55 +0900

    gitignore

---
+++ _posts/2021-04-02-geometric-objects.md
@@ -0,0 +1,4 @@
+---
+title: "[Computer Graphics #3] Geometric Objects"
+---
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,20 +11,26 @@ var store = [{
         "url": "/%EB%85%BC%EB%AC%B8/paper_3dmatch/",
         "teaser": null
       },{
-        "title": "[Computer Graphics] Lec1 빛과 색",
-        "excerpt":"그래픽을 만들어내기 위해서는 빛에 대한 내용을 알고 있어야 합니다. Light 태양과 형광등과 같은 일반적인 광원은 가시광선의 거의 모든 영역을 포함하고있기 때문에 모든 색을 표현할 수 있습니다. 그러나 광원에서 나오는 빛의 색이 한정되어있는 경우도 있기때문에 광원에서 나온 빛이 물체에 반사되어 카메라로 들어올 때의 모습을 표현하기 위해 빛의 특징 또한 알고있어야 합니다....","categories": ["Graphics"],
+        "title": "[Computer Graphics #1] 빛과 색",
+        "excerpt":"그래픽을 만들어내기 위해서는 빛에 대한 내용 또한 알고 있어야 한다. Light 태양과 형광등과 같은 일반적인 광원은 가시광선의 거의 모든 영역을 포함하고 있기 때문에 모든 색을 표현할 수 있다. 그러나 광원에서 나오는 빛의 색이 한정되어있는 경우도 있기 때문에 광원에서 나온 빛이 물체에 반사되어 카메라로 들어올 때의 모습을 표현하기 위해 빛의 특징...","categories": ["Graphics"],
         "tags": [],
         "url": "/graphics/computer-graphics-geometric-objects/",
         "teaser": null
       },{
-        "title": "[Computer Graphics] Lec2 Camera model",
-        "excerpt":"Transformation transform 작업은 이 변환들이 같은 수학적 group이라는 것을 알게되면 여러 작업을 한번에 수행하는데 큰 장점을 얻을 수 있습니다. group이 되기 위한 조건으로는 이 집합은 어떤 operator에 대해 닫혀있어야 합니다. \\(A \\in G\\,and\\,B \\in G \\rightarrow A*B \\in G\\) 다음을 만족하는 identity element I가 존재해야합니다. \\(A*I=I*A=A\\) element $A$는 다음을 만족하는...","categories": ["Graphics"],
+        "title": "[Computer Graphics #2] Camera model",
+        "excerpt":"Transformation transform 작업은 이 변환들이 같은 수학적 group이라는 것을 알게 되면 여러 작업을 한 번에 수행할 수 있다는 큰 장점을 얻을 수 있다. group이 되기 위한 조건으로는 이 집합은 어떤 operator에 대해 닫혀있어야 한다. \\(A \\in G\\,and\\,B \\in G \\rightarrow A*B \\in G\\) 다음을 만족하는 identity element I가 존재해야 한다....","categories": ["Graphics"],
         "tags": [],
         "url": "/graphics/computer-graphics-lec-2-camera-model/",
         "teaser": null
       },{
-        "title": "[Computer Graphics] Lec6 viewing",
-        "excerpt":"Normalization 통일성을 주기 위해 각각 다른 projection 종류에 맞는 projection matrix를 system에서 모두 수행하기 보다 projection은 항상 orthogonal projection을 하도록 만든다. 이 orthogonal projection을 했을 때 원하는 projection의 모습을 나타낼 수 있도록 data의 형태를 바꿔준다. Pipeline View modelview transformation projection transformation perspective division clipping projection 이 5단계 중 앞의 1~3단계에서...","categories": ["Graphics"],
+        "title": "[Computer Graphics #3] Geometric Objects",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/geometric-objects/",
+        "teaser": null
+      },{
+        "title": "[Computer Graphics #5] Viewing - Orthogonal Projection",
+        "excerpt":"Normalization 통일성을 주기 위해 각각 다른 projection 종류에 맞는 projection matrix를 system에서 모두 수행하기보다 projection은 항상 orthogonal projection을 하도록 만든다. 이 orthogonal projection을 했을 때 원하는 projection의 모습을 나타낼 수 있도록 data의 형태를 바꿔준다. Pipeline View modelview transformation projection transformation perspective division clipping projection 이 5단계 중 앞의 1~3단계에서 각각의...","categories": ["Graphics"],
         "tags": [],
         "url": "/graphics/computer-graphics-lec6-viewing/",
         "teaser": null
